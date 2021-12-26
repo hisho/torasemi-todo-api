7eb1cf319e88dfae78e69cf6c195012781b14b3e
y-mabuchi <mabu.you@gmail.com>
2021-12-26 12:54:04 +0900

    modify gitignore

--- pkg/infrastructure/infrastructure.go
+++ pkg/infrastructure/infrastructure.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"os"
 	"time"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -13,12 +14,24 @@ import (
 var db *sql.DB
 
 func NewDB(conf config.DBConfig) *sql.DB {
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true&loc=Local&charset=utf8mb4&multiStatements=true", conf.DBUser, conf.DBPass, conf.DBHost, conf.DBPort, conf.DBName)
-	db, err := sql.Open("mysql", dsn)
+	socketDir, isSet := os.LookupEnv("DB_SOCKET_DIR")
+	if !isSet {
+		socketDir = "/cloudsql"
+	}
+	instanceConnectionName := os.Getenv("CLOUD_SQL_CONNECTION_NAME")
+	var dsn string
+	if instanceConnectionName != "" {
+		dsn = fmt.Sprintf("%s:%s@unix(/%s/%s)/%s?parseTime=true&loc=Local&charset=utf8mb4&&multiStatements=true", conf.DBUser, conf.DBPass, socketDir, instanceConnectionName, conf.DBName)
+	} else {
+		dsn = fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true&loc=Local&charset=utf8mb4&multiStatements=true", conf.DBUser, conf.DBPass, conf.DBHost, conf.DBPort, conf.DBName)
+	}
+
+	dbTemp, err := sql.Open("mysql", dsn)
 	if err != nil {
 		log.Fatalf("action=sql.Open, status=error: %v", err)
 	}
 
+	db = dbTemp
 	if err := db.Ping(); err != nil {
 		log.Fatalf("action=db.Ping, status=error: %v", err)
 	}
